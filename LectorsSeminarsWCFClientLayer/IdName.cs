38be434fa446bc3c822b1f65200a7f965c3e6dc7
Yuriy Pigovsky <pigovsky@gmail.com>
2014-02-08 14:49:48 +0200

    Config was added by VS automatically

---
+++ LectorsSeminarsWCFClientLayer/IdName.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectorsSeminarsWCFClientLayer
+{
+    public abstract class IdName
+    {
+        protected string _Name = null;
+
+        public abstract void RereadName();
+
+        public abstract void SaveName();
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public Int32 Id { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                if (_Name == null)
+                    RereadName();
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                SaveName();
+            }
+        }
+    }
+}
